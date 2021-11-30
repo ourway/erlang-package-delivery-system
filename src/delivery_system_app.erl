0af3a1403f698faa83b7e0572943d81cb316b29e
Farshid Ashouri <farsheed.ashouri@gmail.com>
2021-11-30 12:55:36 +0000

    preparing http2

--- src/delivery_system_app.erl
+++ src/delivery_system_app.erl
@@ -15,11 +15,24 @@ start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
         {'_', [{"/", rest_handler, []}]}
     ]),
-    {ok, _} = cowboy:start_clear(
-        rest_listener,
-        [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}
-    ),
+%%%% http2
+cowboy:start_tls( 
+                  rest_listener,
+                  [
+                      {port, 8443},
+                      {certfile, "/Users/farsheed/Developer/erlang-package-delivery-system/priv/serve.crt"},
+                      {keyfile, "/Users/farsheed/Developer/erlang-package-delivery-system/priv/server.key"}
+                  ],
+                  #{env => #{dispatch => Dispatch}}
+                ),
+
+
+%%%% http1/1
+%%    {ok, _} = cowboy:start_clear(
+%%        rest_listener,
+%%        [{port, 8080}],
+%%        #{env => #{dispatch => Dispatch}}
+%%    ),
     delivery_system_sup:start_link().
 
 stop(_State) ->
